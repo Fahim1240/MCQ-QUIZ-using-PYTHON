85b8c711da66f39f0cdb16c562fa42dd4a670b31
Fahim1240 <emranhossainfahim15@gmail.com>
2023-07-27 16:00:24 +0600

    2nd commit

--- main.py
+++ main.py
@@ -12,3 +12,11 @@ def display_question(question, choices):
         print(f"{i}. {choice}")
     user_choice = int(input("Enter the number of your choice (1, 2, 3, or 4): "))
     return user_choice
+
+def check_answer(user_choice, correct_answer):
+    return user_choice == correct_answer
+
+
+def update_high_score(name, score):
+    with open("high_scores.txt", "a") as file:
+        file.write(f"{name}: {score}\n")
